3ff45c21e62f6258b8cf88547f9207d9794f47ca
Dhara <dhara00719@gmail.com>
2024-02-14 11:31:12 +0400

    add math puzzle

---
+++ Assets/MyGame/MiniGame_Math/Script/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace math
+{
+    public class GameManager : MonoBehaviour
+    {
+        public TMP_InputField inputField;
+        public GameObject objError;
+        // Start is called before the first frame update
+        void Start()
+        {
+            objError.SetActive(false);
+        }
+
+        public void btn_sendMessage()
+        {
+            if(inputField.text.Trim().Length >0)
+            {
+                if(inputField.text.Trim() == "40")
+                {
+                    Debug.Log("win");
+                    objError.SetActive(false);
+                }
+                else
+                {
+                    objError.SetActive(true);
+                }
+            }
+            else
+            {
+                objError.SetActive(true);
+            }
+        }
+    }
+
+}
